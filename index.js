c297b263810cfd20dcc19192d706c2e66cfdca1c
Andrea Vaghi <andrea.vaghi@jobrapido.com>
2016-04-06 17:23:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var axios = require('axios');
+
+var ROOT_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+module.exports = function (options, callback) {
+  var params = {
+    part: 'snippet',
+    key: options.key,
+    q: options.term,
+    type: 'video'
+  };
+
+  axios.get(ROOT_URL, { params: params })
+    .then(function(response) {
+      if (callback) { callback(response.data.items); }
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+};
