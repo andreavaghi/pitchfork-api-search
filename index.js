c9bef1e525fc9b5511ad13fae811950d509bbaef
Andrea Vaghi <andrea.vaghi@jobrapido.com>
2016-04-29 12:43:59 +0200

    add custom useragent

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ var axios = require('axios');
 
 var ROOT_URL = 'http://pitchfork.com/api/v1/search/_ac/';
 
-module.exports = function (options, callback) {
+module.exports = function(options, callback) {
   var params = {
     query: options.query
   };
 
-  axios.get(ROOT_URL, { params: params })
+  axios.get(ROOT_URL, {
+      params: params,
+      headers: {
+        'User-Agent': 'andreavaghi/pitchfork-api-search'
+      }
+    })
     .then(function(response) {
-      if (callback) { callback(response.data); }
+      if (callback) {
+        callback(response.data);
+      }
     })
     .catch(function(error) {
       console.error(error);
